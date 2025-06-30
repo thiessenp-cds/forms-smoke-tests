eb74e2f5f1c4b9f16d7714af89a2958249794c3e
Peter Thiessen <peter.thiessen@cds-snc.ca>
2025-06-30 15:22:15 -0400

    test2

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 // import terminalReport from "cypress-terminal-report/src/installLogsPrinter";
 
 
+
 // Default form URLs. Can override with `FORM_URL` environment variable
 const FORM_STAGING_URL =
   "https://forms-staging.cdssandbox.xyz/en/id/cmcahuj400000k00btz7n4guz";
